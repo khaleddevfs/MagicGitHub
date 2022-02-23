38933700d9ac3a849a79af1643c3184e49ae2ec3
Khaled Dev Fs <khaled.alhajali91@gmail.com>
2022-02-23 12:23:08 +0100

    Magic GitHub

--- Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.magicgithub.api;
+    package com.openclassrooms.magicgithub.api;
 
 import com.openclassrooms.magicgithub.model.User;
 import java.util.List;
@@ -14,9 +14,9 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public List<User> getUsers() {
-        // TODO: A modifier
-        return null;
-    }
+
+        return users;
+         }
 
     /**
      * Generate a random {@link User} and add it {@link FakeApiService#users} list.
@@ -24,7 +24,8 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void generateRandomUser() {
-        // TODO: A modifier
+
+        this.users.add(User.random());
     }
 
     /**
@@ -32,6 +33,7 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void deleteUser(User user) {
-        // TODO: A modifier
+
+        this.users.remove(user);
     }
 }
